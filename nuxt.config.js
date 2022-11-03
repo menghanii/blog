d00ee78b96efd2f02f5a0d573849681c7efc7678
ian <ian.yang@kakaocorp.com>
2022-11-03 15:53:29 +0900

    subfolder false

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,12 +39,11 @@ export default {
   modules: ["@nuxt/content"],
 
   generate: {
+    subFolders: false,
     async routes() {
       const { $content } = require("@nuxt/content");
       const files = await $content({ deep: true }).only(["path"]).fetch();
-      return files.map((file) =>
-        file.path === "/index" ? "/" : "/blog" + file.path
-      );
+      return files.map((file) => (file.path === "/index" ? "/" : file.path));
     },
   },
 
