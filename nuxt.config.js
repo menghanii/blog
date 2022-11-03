64acb4335ea2900347aed213937aec925b919eb7
ian <ian.yang@kakaocorp.com>
2022-11-03 16:12:43 +0900

    del content

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,9 +43,7 @@ export default {
     async routes() {
       const { $content } = require("@nuxt/content");
       const files = await $content({ deep: true }).only(["path"]).fetch();
-      return files.map((file) =>
-        file.path === "/index" ? "/" : "/content" + file.path
-      );
+      return files.map((file) => (file.path === "/index" ? "/" : file.path));
     },
   },
 
