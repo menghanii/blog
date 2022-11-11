5170fbfc3858608ce9282cc3ca6290e508787046
ian <ian.yang@kakaocorp.com>
2022-11-11 16:46:48 +0900

    card h1/2 & indigo-50

--- components/VCard.vue
+++ components/VCard.vue
@@ -1,17 +1,17 @@
 <template>
   <nuxt-link :to="postPath">
     <div class="relative h-24 border-b-1 sm:h-64 sm:rounded-lg sm:border">
-      <div class="w-full h-0 sm:h-2/3 bg-slate-600 rounded-t-lg">
-        <img :src="image" class="object-cover rounded-t-lg h-full" />
+      <div class="w-full h-0 sm:h-1/2 bg-indigo-50 rounded-t-lg">
+        <img :src="image" class="object-cover rounded-t-lg w-full" />
       </div>
-      <div class="p-2 border-t">
-        <div class="font-semibold text-sm sm:text-md">
+      <div class="p-2 border-t h-1/2">
+        <div class="h-1/3 font-semibold text-sm sm:text-md break-all">
           <slot name="title">title</slot>
         </div>
-        <div class="text-xs text-gray-500 mt-1">
+        <div class="h-1/3 text-xs text-gray-500 mt-1 break-all truncate">
           <slot name="subtitle">subtitle</slot>
         </div>
-        <div class="absolute bottom-0 left-2 text-xs">
+        <div class="h-1/3 text-xs relative">
           <slot name="date">date</slot>
         </div>
       </div>
