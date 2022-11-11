a0df2bbb32d6a8d84daaf85ba33b8c9773a5ade3
ian <ian.yang@kakaocorp.com>
2022-11-12 04:29:58 +0900

    width fixed

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="mt-16 text-center text-gray-500 text-xs sm:text-sm">
+    <font-awesome-icon
+      icon="bars"
+      class="cursor-pointer"
+      @click="showSidebar"
+    />
     welcome to my blog
   </div>
 </template>
