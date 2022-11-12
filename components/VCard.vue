abfa0d2871584fbcef0215cdc80d0284df14b70c
ian <ian.yang@kakaocorp.com>
2022-11-13 03:39:46 +0900

    post list height fixed

--- components/VCard.vue
+++ components/VCard.vue
@@ -1,6 +1,6 @@
 <template>
   <nuxt-link :to="postPath">
-    <div class="relative h-24 border-b-1 sm:h-80 sm:rounded-lg sm:border">
+    <div class="relative min-h-24 border-b-1 sm:h-80 sm:rounded-lg sm:border">
       <div class="w-full hidden sm:block sm:h-1/2 rounded-t-lg relative">
         <img :src="image" class="object-cover rounded-t-lg w-full h-full" />
         <!-- <div
@@ -9,14 +9,14 @@
           <slot name="subtitle">subtitle</slot>
         </div> -->
       </div>
-      <div class="p-2 border-t h-full sm:h-1/2">
+      <div class="pt-2 px-2 border-t h-full sm:h-1/2">
         <div class="h-2/5 font-semibold text-md break-all">
           <slot name="title">title</slot>
         </div>
         <div class="h-2/5 text-xs text-gray-500 mt-1 break-all">
           <slot name="subtitle">subtitle</slot>
         </div>
-        <div class="h-1/5 text-xs relative">
+        <div class="h-1/5 text-xs mt-1">
           <slot name="date">date</slot>
         </div>
       </div>
